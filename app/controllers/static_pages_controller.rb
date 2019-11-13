553dfee566e6f94b541e91809f4a6b2d4a191c1a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2019-11-13 07:57:58 +0200

    Change title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def landingpage
-    set_meta_tags title: 'Pseudo conference for software developers'
+    set_meta_tags title: 'Async conference for software developers'
   end
 
   def thanks_for_submitting_presentation
